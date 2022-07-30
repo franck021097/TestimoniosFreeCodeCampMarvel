859224c3dcb2f4fa96db19a127bd7e98f656c941
Francisco C <franck_021097@hotmail.com>
2022-07-30 00:04:00 -0500

    Se termino el primer proyecto del curso de React desde cero, FCC YT

--- src/App.js
+++ src/App.js
@@ -5,44 +5,51 @@ import Testimonio from "./componentes/Testimonio";
 function App() {
   return (
     <div className="App">
-      <div className="contenedor-principal">
-        <h1>freecodecamp:</h1>
-        <Testimonio 
-          imagen='RobertDowneyJr.jpeg'
-          prefijo= 'El es '
-          nombre= 'Robert Downey Jr'
-          puesto= 'Iron Man'
-          frase= 'Yo soy Iron Man'
+      <header>
+        <h1>freecodecamp</h1>
+      </header>
+      <main className="contenedor-principal">
+        <p id="titulo">Testimonios:</p>
+        <Testimonio
+          imagen="RobertDowneyJr.jpeg"
+          prefijo="El es "
+          nombre="Robert Downey Jr"
+          puesto="Iron Man"
+          frase="Yo soy Iron Man"
         />
-        <Testimonio 
-          imagen='LabrujaScarlata.jpg'
-          prefijo= 'Ella es '
-          nombre= 'Elizabeth Olsen <3'
-          puesto= 'La Bruja Scarlata'
-          frase= 'Rompes las reglas y te conviertes en un héroe… Yo lo hago y me vuelvo el enemigo… No suena justo ¿verdad?.'
+        <Testimonio
+          imagen="LabrujaScarlata.jpg"
+          prefijo="Ella es "
+          nombre="Elizabeth Olsen ♥"
+          puesto="La Bruja Scarlata"
+          frase="Rompes las reglas y te conviertes en un héroe… Yo lo hago y me vuelvo el enemigo… No suena justo ¿verdad?."
         />
-        <Testimonio 
-          imagen='AmericaChavez.jpg'
-          prefijo= 'Ella es '
-          nombre= 'Xochitl Gomez'
-          puesto= 'Ms America'
-          frase= 'Regla número uno del viaje en el multiverso: no sabes nada.'
+        <Testimonio
+          imagen="AmericaChavez.jpg"
+          prefijo="Ella es "
+          nombre="Xochitl Gomez"
+          puesto="Ms America"
+          frase="Regla número uno del viaje en el multiverso: no sabes nada."
         />
-        <Testimonio 
-          imagen='DoctorStrange.jpg'
-          prefijo= 'El es '
-          nombre= 'Benedict Cumberbatch'
-          puesto= 'Doctor Strange'
-          frase= 'Sé bien lo que es sufrir, el dolor es un viejo amigo.'
+        <Testimonio
+          imagen="DoctorStrange.jpg"
+          prefijo="El es "
+          nombre="Benedict Cumberbatch"
+          puesto="Doctor Strange"
+          frase="Sé bien lo que es sufrir, el dolor es un viejo amigo."
         />
-        <Testimonio 
-          imagen='Groot.jpg'
-          prefijo= 'El es '
-          nombre= 'Vin Diesel'
-          puesto= 'Groot'
-          frase= 'Yo soy Groot.'
+        <Testimonio
+          imagen="Groot.jpg"
+          prefijo="El es "
+          nombre="Vin Diesel"
+          puesto="Groot"
+          frase="Yo soy Groot."
         />
-      </div>
+      </main>
+
+      <footer>
+        <p>Todos los derechos reservados © </p>
+      </footer>
     </div>
   );
 }
