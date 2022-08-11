b2626e9e95158ab9080ddc4788f4ee1b0ae9304e
Francisco C <franck_021097@hotmail.com>
2022-08-11 18:45:25 -0500

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
 // import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Testimonio from "./componentes/Testimonio";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>freecodecamp</h1>
-      </header>
-      <main className="contenedor-principal">
-        <p id="titulo">Testimonios:</p>
-        <Testimonio
-          imagen="RobertDowneyJr.jpeg"
-          prefijo="El es "
-          nombre="Robert Downey Jr"
-          puesto="Iron Man"
-          frase="Yo soy Iron Man"
-        />
-        <Testimonio
-          imagen="LabrujaScarlata.jpg"
-          prefijo="Ella es "
-          nombre="Elizabeth Olsen ♥"
-          puesto="La Bruja Scarlata"
-          frase="Rompes las reglas y te conviertes en un héroe… Yo lo hago y me vuelvo el enemigo… No suena justo ¿verdad?."
-        />
-        <Testimonio
-          imagen="AmericaChavez.jpg"
-          prefijo="Ella es "
-          nombre="Xochitl Gomez"
-          puesto="Ms America"
-          frase="Regla número uno del viaje en el multiverso: no sabes nada."
-        />
-        <Testimonio
-          imagen="DoctorStrange.jpg"
-          prefijo="El es "
-          nombre="Benedict Cumberbatch"
-          puesto="Doctor Strange"
-          frase="Sé bien lo que es sufrir, el dolor es un viejo amigo."
-        />
-        <Testimonio
-          imagen="Groot.jpg"
-          prefijo="El es "
-          nombre="Vin Diesel"
-          puesto="Groot"
-          frase="Yo soy Groot."
-        />
-      </main>
-
-      <footer>
-        <p>Todos los derechos reservados © </p>
-      </footer>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>freecodecamp</h1>
+        </header>
+        <main className="contenedor-principal">
+          <p id="titulo">Testimonios:</p>
+          <Testimonio
+            imagen="RobertDowneyJr.jpeg"
+            prefijo="El es "
+            nombre="Robert Downey Jr"
+            puesto="Iron Man"
+            frase="Yo soy Iron Man"
+          />
+          <Testimonio
+            imagen="LabrujaScarlata.jpg"
+            prefijo="Ella es "
+            nombre="Elizabeth Olsen ♥"
+            puesto="La Bruja Scarlata"
+            frase="Rompes las reglas y te conviertes en un héroe… Yo lo hago y me vuelvo el enemigo… No suena justo ¿verdad?."
+          />
+          <Testimonio
+            imagen="AmericaChavez.jpg"
+            prefijo="Ella es "
+            nombre="Xochitl Gomez"
+            puesto="Ms America"
+            frase="Regla número uno del viaje en el multiverso: no sabes nada."
+          />
+          <Testimonio
+            imagen="DoctorStrange.jpg"
+            prefijo="El es "
+            nombre="Benedict Cumberbatch"
+            puesto="Doctor Strange"
+            frase="Sé bien lo que es sufrir, el dolor es un viejo amigo."
+          />
+          <Testimonio
+            imagen="Groot.jpg"
+            prefijo="El es "
+            nombre="Vin Diesel"
+            puesto="Groot"
+            frase="Yo soy Groot."
+          />
+        </main>
+    
+        <footer>
+          <p>Todos los derechos reservados © </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css'
 
-function Testimonio(props){
-  return(
-    <div className='contenedor-testimonio'>
-      <img 
-        className='imagen-testimonio'
-        src={require(`../imagenes/${props.imagen}`)}
-        alt='imagen de Iron Man'
-      />
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.prefijo}{props.nombre}</p>
-        <p className='cargo-testimonio'>Su puesto es <strong>{props.puesto}</strong></p>
-        <p className='texto-testimonio'>"{props.frase}"</p>
+class Testimonio extends React.Component{
+  render(){
+    return(
+      <div className='contenedor-testimonio'>
+        <img 
+          className='imagen-testimonio'
+          src={require(`../imagenes/${this.props.imagen}`)}
+          alt={`Foto de ${this.props.nombre}`} />
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>{this.props.prefijo}{this.props.nombre}</p>
+          <p className='cargo-testimonio'>Su puesto es <strong>{this.props.puesto}</strong></p>
+          <p className='texto-testimonio'>"{this.props.frase}"</p>
+        </div>
       </div>
-    </div>
-  );
+    );   
+  }
 }
+
 
 export default Testimonio;
